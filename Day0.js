f161d14f11d36bf55f6f9ec8aac7b65d4a8c4e63
Derek <droemhildt28@gmail.com>
2019-12-30 15:00:43 -0600

    Setup for Day 0

---
+++ Day0.js
@@ -0,0 +1,12 @@
+/**
+ * @Author: Derek Roemhildt
+ * @Date: 12/30/2019
+ */
+
+//HackerRank 10 Days of Javascript Day 0
+
+//Hello World
+function greeting()
+{
+    document.write("Hello, World!");
+}
