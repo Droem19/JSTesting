7ab703261e30cf5f1bb32dd7a56e76a22392ec66
Derek <droemhildt28@gmail.com>
2019-12-30 15:32:15 -0600

    Day 1 Completion

--- Day1.js
+++ Day1.js
@@ -4,3 +4,26 @@
  */
 
 //HackerRank 10 Days of Javascript Day 1
+
+//Create a function that returns the factorial of any passed integer
+function factorial(n){
+    for(var i = n -1; i > 0; i--){
+        n = (n * (i));
+    }
+    return n;
+}
+
+//Create a function that returns the area of a circle with a given radius
+//Area = PI * R^2
+const PI = Math.PI;
+function area(r){
+    var area = PI * (r * r);
+    return area;
+}
+
+//Create a function that returns the perimeter of a circle with a given radius
+//Perimeter = 2 * PI * R
+function perimeter(r){
+    var perim = 2 * PI * r;
+    return perim;
+}
